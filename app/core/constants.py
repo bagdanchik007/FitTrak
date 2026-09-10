03bd898596f84cfaa17b10d8417a52d8820cf528
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-10 23:24:40 +0200

    feat: add application constants

---
+++ app/core/constants.py
@@ -0,0 +1,35 @@
+"""Application-wide constants."""
+
+# Pagination
+DEFAULT_PAGE_SIZE = 20
+MAX_PAGE_SIZE = 100
+
+# Token types
+TOKEN_TYPE_ACCESS = "access"
+TOKEN_TYPE_REFRESH = "refresh"
+
+# Muscle groups (common values for validation / filtering)
+MUSCLE_GROUPS = [
+    "chest",
+    "back",
+    "shoulders",
+    "biceps",
+    "triceps",
+    "legs",
+    "core",
+    "glutes",
+    "full_body",
+    "cardio",
+]
+
+# Equipment types
+EQUIPMENT_TYPES = [
+    "barbell",
+    "dumbbell",
+    "machine",
+    "cable",
+    "bodyweight",
+    "kettlebell",
+    "resistance_band",
+    "other",
+]
