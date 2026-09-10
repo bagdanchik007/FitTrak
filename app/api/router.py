765c59a9dfe19cf2f8fe18ed6f1f9a58fb3f2952
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-10 23:24:43 +0200

    refactor: organize API router

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1 import auth, exercises, users, workouts
+from app.api.v1 import auth, exercises, health, progress, users, workouts
 
 api_router = APIRouter()
 
@@ -8,3 +8,5 @@ api_router.include_router(auth.router)
 api_router.include_router(users.router)
 api_router.include_router(exercises.router)
 api_router.include_router(workouts.router)
+api_router.include_router(progress.router)
+api_router.include_router(health.router)
