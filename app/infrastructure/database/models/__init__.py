e93eb41e588ff3a0ea563f327ee6119026e7feeb
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-20 23:52:30 +0200

    feat(db): register new models in models package exports

--- app/infrastructure/database/models/__init__.py
+++ app/infrastructure/database/models/__init__.py
@@ -1,10 +1,19 @@
 from app.infrastructure.database.models.user import UserModel
 from app.infrastructure.database.models.exercise import ExerciseModel
 from app.infrastructure.database.models.workout import WorkoutModel, WorkoutSetModel
+from app.infrastructure.database.models.goal import GoalModel
+from app.infrastructure.database.models.body_weight import BodyWeightModel
+from app.infrastructure.database.models.template import WorkoutTemplateModel, WorkoutTemplateItemModel
+from app.infrastructure.database.models.preference import UserPreferenceModel
 
 __all__ = [
     "UserModel",
     "ExerciseModel",
     "WorkoutModel",
     "WorkoutSetModel",
+    "GoalModel",
+    "BodyWeightModel",
+    "WorkoutTemplateModel",
+    "WorkoutTemplateItemModel",
+    "UserPreferenceModel",
 ]
