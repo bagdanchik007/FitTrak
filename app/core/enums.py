24d95c401b17c09618f37c5b6d8eb2a406c0a13b
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-25 23:43:10 +0200

    feat(core): add weight unit mood and activity type enums

---
+++ app/core/enums.py
@@ -0,0 +1,22 @@
+"""Shared string enums as plain constants (lightweight)."""
+
+
+class WeightUnit:
+    KG = "kg"
+    LBS = "lbs"
+
+
+class Mood:
+    HAPPY = "happy"
+    NEUTRAL = "neutral"
+    TIRED = "tired"
+    MOTIVATED = "motivated"
+    SORE = "sore"
+    GREAT = "great"
+
+
+class ActivityType:
+    WORKOUT = "workout"
+    BODY_WEIGHT = "body_weight"
+    JOURNAL = "journal"
+    GOAL = "goal"
